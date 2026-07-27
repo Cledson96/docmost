c1606a5141b0ad02a37e2f516ffb1134360edd6f
cledson <cledson.santos@gobrax.com>
2026-07-27 10:24:09 -0300

    feat:personal modulo

--- apps/server/src/core/space/space.module.ts
+++ apps/server/src/core/space/space.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SpaceService } from './services/space.service';
 import { SpaceController } from './space.controller';
+import { PersonalSpaceController } from './personal-space.controller';
 import { SpaceMemberService } from './services/space-member.service';
 
 @Module({
   imports: [],
-  controllers: [SpaceController],
+  controllers: [SpaceController, PersonalSpaceController],
   providers: [SpaceService, SpaceMemberService],
   exports: [SpaceService, SpaceMemberService],
 })
